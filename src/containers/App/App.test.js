445a5cc159418b8bd69b3defe991ff9596b6cfe0
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 11:50:13 -0700

    Add mapStateToProps and mapDispatchToProps tests on App

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { mapStateToProps, mapDispatchToProps } from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+jest.mock('../../thunks/fetchPresidents')
+
+describe('App', () => {
+  const mockProps = {
+    isLoading: false,
+    hasErrored: '',
+    presidents: [{ president: 'Obama' }]
+  }
+  
+  describe('mapStateToProps', () => {
+    it('should return a props object with isLoading, hasErrored, and the presidents array', () => {
+      const mockState = {
+        ...mockProps,
+        assholes: []
+      }
+      const expected = mockProps
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should dispatch fetchPresidents thunk', () => {
+      const mockDispatch = jest.fn()
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      mappedProps.fetchPresidents()
+      expect(mockDispatch).toHaveBeenCalled()
+    })
+  })
+})
