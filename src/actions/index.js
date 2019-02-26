ec00b398d07d8ba9645344738a09228adc7db50e
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 09:15:41 -0700

    Add isLoading, hasErrored, and setPresidents actions

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  isLoading: bool
+})
+ 
+export const hasErrored = (message) => ({
+  type: 'HAS_ERRORED',
+  message
+})
+
+export const setPresidents = (presidents) => ({
+  type: 'SET_PRESIDENTS',
+  presidents
+})
