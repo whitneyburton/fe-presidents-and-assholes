06c41865baa04e52de2e83dd943ac1777594a995
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 10:12:13 -0700

    Delete fetchData console.log

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,6 @@
 export const fetchData = async (url) => {
   try {
     const response = await fetch(url)
-    console.log(response)  
     return response; 
   } catch (error) {
     throw new Error(error);
