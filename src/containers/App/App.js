fa4a16e82a2192d61812cd0df6aada09fe2ea4e8
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 10:12:38 -0700

    Add componentDidMount fetch call and hasErrored/isLoading notices to App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { setPresidents } from '../../actions';
+import { fetchPresidents } from '../../thunks/fetchPresidents';
 
-class App extends Component {
+export class App extends Component {
+
+  componentDidMount = () => {
+    const url = 'http://localhos:3001/api/v1/presidents'
+    this.props.fetchPresidents(url)
+  }
+
   render() {
+    const { hasErrored, isLoading, presidents } = this.props;
+
     return (
       <div className="App">
-
+        <h1>Presidents + Assholes</h1>
+        {hasErrored && <h1>Sorry, there was an error fetching the presidents.</h1>}
+        {isLoading && <h1>Loading...</h1>}
       </div>
     );
   }
 }
 
-export default App;
+export const mapStateToProps = (state) => ({
+  isLoading: state.isLoading,
+  hasErrored: state.hasErrored,
+  presidents: state.presidents
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  fetchPresidents: (url) => dispatch(fetchPresidents(url)),
+  setPresidents: (presidents) => dispatch(setPresidents(presidents))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
