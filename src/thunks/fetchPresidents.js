a62a96618fca75dbf2c8ebf7dfc5c2b906be0b8e
Whitney Burton <whitneyburton9@gmail.com>
2019-02-26 10:13:14 -0700

    Adjust fetchPresidents to fetch instead of fetchData

--- src/thunks/fetchPresidents.js
+++ src/thunks/fetchPresidents.js
@@ -1,16 +1,18 @@
 import { isLoading, hasErrored, setPresidents } from '../actions'
-import { fetchData } from '../api/fetchData';
 
 export const fetchPresidents = (url) => {
   return async (dispatch) => {
     try {
       dispatch(isLoading(true));
-      const response = await fetchData(url);
-      if (!response.ok) throw new Error(response.statusText)
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
       dispatch(isLoading(false))
       const data = await response.json();
       dispatch(setPresidents(data))
     } catch (error) {
+      dispatch(isLoading(false))
       dispatch(hasErrored(error.message))
     }
   }
